50d1c3281b4e8651893c291a6336f797f37e959e
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2023-02-21 17:38:47 +0200

    Add: initialize socket io objectW

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const port = 8080;
 const server = http.createServer(express);
 const wss = new socket.Server({ server });
 
+const { Server } = require("socket.io");
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
 wss.on("connection", (ws) => {
   ws.on("message", (data) => {
     console.log("got message " + data);
